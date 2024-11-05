3f2d709d1914931af438d4803d449713daf1f1df
kanghaol <kanghaol@buffalo.edu>
2024-11-05 01:06:27 -0500

    finish parsing the columns for content based filtering

--- util/csvParser.py
+++ util/csvParser.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import ast
 
+
+def parse_genres(x):
+    try:
+        genres = ast.literal_eval(x)
+        genres_list = []
+        for g in genres:
+            genres_list.append(g['name'])
+        # concate into a single string instead of a list of genres
+        result = ''.join(genres_list)
+        return result
+    except (TypeError):
+        return ""
 def ParseMovieData():
     csv_file_path = 'Data/movies_metadata.csv'
     
@@ -11,6 +24,9 @@ def ParseMovieData():
     df['vote_count'] = pd.to_numeric(df['vote_count'], errors='coerce')
     
     content_df = df[['title', 'genres', 'overview', 'tagline', 'production_companies']]
-
+    content_df = content_df.fillna('')
+    
+    content_df['genres'] = content_df['genres'].apply(parse_genres)
+    content_df['content'] = content_df['overview'] + ' ' + content_df['genres'] + ' ' + content_df['overview'] + ' ' + content_df['tagline'] + ' ' + content_df['production_companies']
     return content_df
 ParseMovieData()
