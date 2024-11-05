4cc28bcc5b22832d47027d66a463c0aaa2ad96aa
kanghaol <kanghaol@buffalo.edu>
2024-11-05 00:46:29 -0500

    pick out the columns need for content based filtering

--- util/csvParser.py
+++ util/csvParser.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 def ParseMovieData():
     csv_file_path = 'Data/movies_metadata.csv'
-    df = pd.read_csv(csv_file_path)
-    print(df)
-    pass
+    
+    # Load the dataset with low_memory=False to avoid DtypeWarning
+    df = pd.read_csv(csv_file_path, low_memory=False)
+    
+    # Ensure 'vote_average' and 'vote_count' columns are numeric
+    df['vote_average'] = pd.to_numeric(df['vote_average'], errors='coerce')
+    df['vote_count'] = pd.to_numeric(df['vote_count'], errors='coerce')
+    
+    content_df = df[['title', 'genres', 'overview', 'tagline', 'production_companies']]
 
+    return content_df
 ParseMovieData()
