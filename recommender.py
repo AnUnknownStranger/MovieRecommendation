904817510d53026b77aa341edc18bd2fef1cc943
AnUnknownStranger <105066952+AnUnknownStranger@users.noreply.github.com>
2024-11-06 02:15:21 -0500

    small bug fix

--- recommender.py
+++ recommender.py
@@ -67,7 +67,7 @@ def MakeRecommendation(movie_title):
 
     # Later, we load the precomputed data and generate recommendations
     precomputed_factors = torch.load("Movie_factor.pt")
-    recommended_indices = Recommender(Movie_idx=movie_idx, movie_factors=precomputed_factors, number=20, data=data)
+    recommended_indices = Recommender(Movie_idx=movie_idx, movie_factors=precomputed_factors, number=10, data=data)
 
     recommended_movies = TopRecommendations(data,recommended_indices,5)
     
